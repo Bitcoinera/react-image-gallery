d04f4a2346781d61556c236224792559721b0b8e
anagutjor <anagutjor@gmail.com>
2019-09-16 20:57:25 +0200

    fix grid with react

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,9 +1,11 @@
-import './ImagesList.css'
 import React from 'react'
+import './ImagesList.css'
+import './ImageCard'
+import ImageCard from './ImageCard'
 
 const ImagesList = (props) => {
-    const images = props.images.map( ({id, urls, description}) => 
-    <img key={id} src={urls.small} alt={description}></img>)
+    const images = props.images.map( (image) => 
+    <ImageCard key={image.id} image={image} />)
     
     return (
         <div className={'images-list'}>
